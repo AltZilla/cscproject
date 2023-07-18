cdd2c52a81190243c538da5761a90c2ce72aacc5
Sunil L <9e37shresht@gmail.com>
2023-07-18 21:05:07 +0530

    ask user for password

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ import sys
 ctk.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-conn = mysql.connect(host='localhost', user='root', password='syed', database='project')
-if conn.is_connected():
-    cursor = conn.cursor()
-else:
+password = ctk.CTkInputDialog(title = "Login", text = "Enter password for `root` user").get_input() # this will make a window and ask user for password, and wait for a response
+
+conn = mysql.connect(host='localhost', user='root', password=password)
+
+if not conn.is_connected():
     print("Failed to connect to MySQL server")
     sys.exit() # this quits the program
+    
+cur = conn.cursor()
+cur.execute("CREATE DATABASE IF NOT EXISTS project") # Create the database if it doesnt exist
+cur.execute("use project") # Move cursor to project database
+
 
 root = ctk.CTk()
 root.geometry("1100x580") # set the window dimensions in pixels
